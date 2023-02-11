79e0fa78a679a6adebe622806e4a0710ba52ba6d
KirioXX <wunderlich.ben@gmail.com>
2023-02-11 18:54:02 +0000

    fix: :rocket: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import wasm from 'vite-plugin-wasm'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  base: '/interactive-wasm-pdf/'
 })
